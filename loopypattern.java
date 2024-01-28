0ecc6e3abbfb2caf2d1a9d58742e2d13816132a5
Shubham <40598861+vijit101@users.noreply.github.com>
2024-01-29 00:08:51 +0530

    Star Pattern Recursive
    Experimented with star patterns without using loops in a recursive format

---
+++ loopypattern.java
@@ -0,0 +1,20 @@
+public class loopypattern {
+    public static void main(String[] args) {
+        LoopPrintRecursion(5);
+
+    }
+
+    private static void LoopPrintRecursion(int n) {
+
+        if (n == 0) {
+            return;
+        }
+        // System.out.println("Iteration" + n);
+        for (int i = 0; i <= n; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        LoopPrintRecursion(n - 1);
+
+    }
+}
