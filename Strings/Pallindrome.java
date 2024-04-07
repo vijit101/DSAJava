9782edfad802ced5efbe9c153919de62e29f9700
Shubham <40598861+vijit101@users.noreply.github.com>
2024-04-08 00:05:50 +0530

    Pallindrome
    A simple Pallindrome problem solved

---
+++ Strings/Pallindrome.java
@@ -0,0 +1,25 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class Pallindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String mystr = sc.next();
+        boolean pallin = checkPallindrome(mystr);
+        System.out.println(pallin);
+
+    }
+
+    public static boolean checkPallindrome(String getStr) {
+        boolean isfound = true;
+        int j = getStr.length() - 1;
+        for (int i = 0; i < (getStr.length() / 2) + 1; i++) {
+            if (getStr.charAt(i) != getStr.charAt(j) && i <= j) {
+                return false;
+            }
+            j--;
+        }
+        return isfound;
+    }
+}
