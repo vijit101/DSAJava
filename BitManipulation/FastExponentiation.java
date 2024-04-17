a718f73a273e062b9340f1176ad702d3f092fdbd
Shubham <40598861+vijit101@users.noreply.github.com>
2024-04-17 20:35:39 +0530

    logic map
    Added code

--- BitManipulation/FastExponentiation.java
+++ BitManipulation/FastExponentiation.java
@@ -1,5 +1,12 @@
 public class FastExponentiation {
     public static void main(String[] args) {
+        // logic is that binaries are log n + 1 hence
+        // run loop as per power but in binary
+
+        fastExponen(num, pow);
+    }
+
+    public static int fastExponen(int num, int pow) {
 
     }
 }
