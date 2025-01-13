c3a3fd1928f9b2f13a12ad53c31030d855c86be2
Shubham <40598861+vijit101@users.noreply.github.com>
2025-01-13 18:05:53 +0530

    upodated bin search
    bin search

---
+++ Searching/ClosestBinSearch.java
@@ -0,0 +1,41 @@
+public class ClosestBinSearch {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 10 };
+        int index = binSearch(5, arr);
+        System.out.println(index);
+    }
+
+    public static int binSearch(int key, int[] arry) {
+        int indx = -1;
+        int start = 0;
+        int end = arry.length;
+        int midpt = arry.length / 2;
+        inr diff = -1;
+        for (int i = 0; i <= (arry.length) / 2; i++) {
+            if (key == arry[midpt]) {
+                indx = midpt;
+                return indx;
+            } else if (key < arry[midpt]) {
+                end = midpt;
+                midpt = (midpt + start) / 2;
+                diff = key - arry[midpt];
+                diff = Math.abs(diff);
+
+            } else if (key > arry[midpt]) {
+                start = midpt;
+                midpt = (midpt + end) / 2;
+
+            }
+        }
+        return indx;
+    }
+
+    public static int search(int[] nums, int target) {
+        // Write your code here.
+        int indx = -1;
+        return indx;
+    }
+
+}
+
+
