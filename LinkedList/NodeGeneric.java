6af666e76f3c02066e8ece5f3d39d6cc197b898e
Shubham <40598861+vijit101@users.noreply.github.com>
2025-01-14 11:49:18 +0530

    LL generic implementation
    Linked list

---
+++ LinkedList/NodeGeneric.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+public class NodeGeneric<T> {
+    public T data;
+    public NodeGeneric<T> next;
+
+
+    public NodeGeneric(){
+        
+    }
+
+    public NodeGeneric(T myData) {
+        this.data = myData;
+    }
+
+    // public void AddNewNode(NodeGeneric<T> head , T Newdata){
+    //     NodeGeneric<T> nd = new NodeGeneric<T>();
+    //     nd.data = Newdata;
+    // }
+}
