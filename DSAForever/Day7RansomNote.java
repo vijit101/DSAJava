2c743fb23fa55cdc60281aa815560e5b883657f6
Shubham <40598861+vijit101@users.noreply.github.com>
2025-08-31 21:05:11 +0530

    Ransom Note

---
+++ DSAForever/Day7RansomNote.java
@@ -0,0 +1,12 @@
+package DSAForever;
+
+public class Day7RansomNote {
+     public boolean canConstruct(String ransomNote, String magazine) {
+    int[] count = new int[26];
+    for (char c : magazine.toCharArray()) count[c - 'a']++;
+    for (char c : ransomNote.toCharArray()) {
+        if (--count[c - 'a'] < 0) return false;
+    }
+    return true;
+}
+}
