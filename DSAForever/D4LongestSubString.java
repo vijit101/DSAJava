fb71cfc9818585eb6818e360d767df22dae23243
Shubham <40598861+vijit101@users.noreply.github.com>
2026-04-20 16:31:26 +0530

    added a solution
    - updated the solution for char replacement problem

--- DSAForever/D4LongestSubString.java
+++ DSAForever/D4LongestSubString.java
@@ -46,4 +46,28 @@ public class D4LongestSubString {
 
         return ans;
     }
+
+
+    public int characterReplacement(String s, int k) {
+    int[] freq = new int[26];
+    int left = 0;
+    int maxFreq = 0;
+    int ans = 0;
+
+    for (int right = 0; right < s.length(); right++) {
+        char c = s.charAt(right);
+        freq[c - 'A']++;
+
+        maxFreq = Math.max(maxFreq, freq[c - 'A']);
+
+        while ((right - left + 1) - maxFreq > k) {
+            freq[s.charAt(left) - 'A']--;
+            left++;
+        }
+
+        ans = Math.max(ans, right - left + 1);
+    }
+
+    return ans;
+}
 }
