9ff387460baab620a02bf278d2858f13d5a067bd
Shubham <40598861+vijit101@users.noreply.github.com>
2026-06-22 17:53:41 +0530

    update

--- DSAForever/D6PermutationInString.java
+++ DSAForever/D6PermutationInString.java
@@ -1,5 +1,7 @@
 package DSAForever;
 
+import java.util.Arrays;
+
 public class D6PermutationInString {
 
     public static void main(String[] args) {
@@ -9,11 +11,31 @@ public class D6PermutationInString {
     public boolean checkInclusion(String s1, String s2) {
         boolean ans = false;
         int[] freq = new int[26];
-        int subLength = s1.length();
+        int end = 0;
+        for (char elem : s1.toCharArray()) {
+            freq[elem-'a']++;
+        }
 
-        for (int i = 0; i < subLength; i++) {
+        int s2Len = s2.length();
+        char[] s2CharArray = s2.toCharArray();
+        int[] freqs2 = new int[26];
+
+        for (int i = 0; i < s2Len; i++) {
             
+            char EndthChar = s2CharArray[end];
+            while(freqs2[EndthChar-'a'] < freq[EndthChar -'a'] && freq[EndthChar -'a'] != 0){
+                freqs2[EndthChar -'a']++;
+                end++;
+                if(Arrays.equals(freq, freqs2)){
+                    ans = true;
+                }
+            }
+            
+
+            // might need to do soemthing where I do -1 from freq and then i ++ that way i can use end but not miss any combos 
+            freqs2[s2CharArray[i] - 'a'] -- ; 
         }
         return ans;
     }
+    
 }
